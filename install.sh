9f890814c26fd7036526a69573d4761f47268287
Simon van der Veldt <simon.vanderveldt@gmail.com>
2016-05-21 19:38:19 +0200

    Enable per package install script

--- install.sh
+++ install.sh
@@ -3,4 +3,9 @@
 # See https://lists.gnu.org/archive/html/help-stow/2013-04/msg00008.html
 set -x
 
-stow -v --no-folding --target=/ $1
+stow -v --no-folding --ignore install.sh --ignore "$1.*" --target=/ $1
+
+# Run package specific install script if it exists
+if [ -x "$1/install.sh" ]; then
+  $1/install.sh
+fi
