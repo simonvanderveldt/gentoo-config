86dc2dc8ee7151ac53ccef64bb8e6239df160aea
Simon van der Veldt <simon.vanderveldt@gmail.com>
2016-07-28 19:59:23 +0200

    Properly quote all variables

--- lm_sensors/install.sh
+++ lm_sensors/install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 # Install motherboard specific files
+set -x
+
 BOARD_NAME_FULL=$(cat /sys/devices/virtual/dmi/id/board_name)
 BOARD_NAME=${BOARD_NAME_FULL%%' '*}
 BOARD_NAME=${BOARD_NAME,,}
 BASEDIR=$(dirname "$0")
 
-if [ -d "$BASEDIR/lm_sensors.$BOARD_NAME" ] ; then
-    cd $BASEDIR; stow -v --no-folding --target=/ "lm_sensors.$BOARD_NAME"
+if [ -d "${BASEDIR}/lm_sensors.${BOARD_NAME}" ] ; then
+    cd "${BASEDIR}"; stow -v --no-folding --target=/ "lm_sensors.${BOARD_NAME}"
 fi
