8fe1c1187497c7f2f9523c0ff0de516c180f4da4
Simon van der Veldt <simon.vanderveldt@gmail.com>
2017-06-18 12:08:14 +0200

    [gentoo-config] Remove add user to groups
    The groups don't exist yet when initially installing the config so it doesn't work :P

--- gentoo-config/install.sh
+++ gentoo-config/install.sh
@@ -5,6 +5,3 @@ BASEDIR=$(dirname "$0")
 
 # fix permissions for sudoers.d file
 chown root:root "${BASEDIR}/etc/sudoers.d/wheel"
-
-# Add user `simon` to the relevant groups
-usermod -a -G wheel,kvm,libvirt,docker,scanner simon
