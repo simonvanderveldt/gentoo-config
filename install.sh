99e6292902805afacc57df87e48d1af26465ceb8
Simon van der Veldt <simon.vanderveldt@gmail.com>
2017-10-16 11:51:29 +0200

    Exit if no argument is passed to (un)install script

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 # See https://lists.gnu.org/archive/html/help-stow/2013-04/msg00008.html
 set -x
 
+if [ -z "$1" ] ; then
+  echo "No argument supplied, exiting"
+  exit 1
+fi
+
 stow -v --no-folding --ignore install.sh --target=/ "${1}"
 
 # Run package specific install script if it exists
--- uninstall.sh
+++ uninstall.sh
@@ -3,4 +3,9 @@
 # See https://lists.gnu.org/archive/html/help-stow/2013-04/msg00008.html
 set -x
 
+if [ -z "$1" ] ; then
+  echo "No argument supplied, exiting"
+  exit 1
+fi
+
 stow -v -D --no-folding --ignore install.sh --target=/ "${1}"
