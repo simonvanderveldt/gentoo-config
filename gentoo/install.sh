f9e3257d30f3c38188a8aa1f8631647a64bee422
Simon van der Veldt <simon.vanderveldt@gmail.com>
2019-01-24 14:22:40 +0100

    gentoo: use eselect to set profile

--- gentoo/install.sh
+++ gentoo/install.sh
@@ -3,8 +3,8 @@ set -x
 
 BASEDIR=$(dirname "$0")
 
-# fix permissions for sudoers.d file
-chown root:root "${BASEDIR}/etc/sudoers.d/wheel"
+# Set profile
+eselect profile dantrell-gnome-3-26:default/linux/amd64/17.0/desktop/gnome/3.26
 
 # Set locale
 eselect locale set en_US.utf8
@@ -12,3 +12,6 @@ env-update
 
 # Set GCC version
 eselect gcc set x86_64-pc-linux-gnu-7.3.0
+
+# Fix permissions for sudoers.d file
+chown root:root "${BASEDIR}/etc/sudoers.d/wheel"
