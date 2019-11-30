3ea03fc04ded53499e3eab4e3918f0311ed0674d
Simon van der Veldt <simon.vanderveldt@gmail.com>
2019-11-30 22:10:16 +0100

    gentoo/portage: Update to GCC 9.2.0

--- gentoo/install.sh
+++ gentoo/install.sh
@@ -8,7 +8,7 @@ locale-gen
 env-update
 
 # Set GCC version
-eselect gcc set x86_64-pc-linux-gnu-7.3.0
+eselect gcc set x86_64-pc-linux-gnu-9.2.0
 
 # Fix permissions for sudoers.d file
 chown root:root "${BASEDIR}/etc/sudoers.d/wheel"
