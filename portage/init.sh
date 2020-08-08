2011626c3d7591c5bf069ab5d946b1cbbfab9c21
Simon van der Veldt <simon.vanderveldt@gmail.com>
2020-08-08 21:08:25 +0200

    portage: Upgrade to GNOME 3.36

--- portage/init.sh
+++ portage/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -x
 
-eselect profile set dantrell-gnome-3-32:default/linux/amd64/17.0/desktop/gnome/3.32
+eselect profile set dantrell-gnome-3-36:default/linux/amd64/17.0/desktop/gnome/3.36
