14ed2f3fe1a20a5e639399988bb02579b586c376
Simon van der Veldt <simon.vanderveldt@gmail.com>
2022-01-03 12:02:05 +0100

    Switch to GCC 11

--- gentoo/init.sh
+++ gentoo/init.sh
@@ -6,7 +6,7 @@ locale-gen
 env-update
 
 # Set GCC version
-eselect gcc set x86_64-pc-linux-gnu-9.2.0
+eselect gcc set x86_64-pc-linux-gnu-11.2.0
 
 # Fix permissions for sudoers.d/wheel file
 chown root:root /etc/sudoers.d/wheel
