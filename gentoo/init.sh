15ac2edc19cfe42512637b679ae0a8d2eec4a7fe
Simon van der Veldt <simon.vanderveldt@gmail.com>
2022-06-11 11:13:16 +0200

    gentoo: Configure groups for my user

--- gentoo/init.sh
+++ gentoo/init.sh
@@ -10,3 +10,6 @@ eselect gcc set x86_64-pc-linux-gnu-11.2.0
 
 # Fix permissions for sudoers.d/wheel file
 chown root:root /etc/sudoers.d/wheel
+
+# Add user to relevant groups
+usermod -a -G dialout,docker,kvm,libvirt,portage,scanner,usb,video,wheel simon
