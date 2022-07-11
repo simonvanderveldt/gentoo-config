0f16d279da2775462c3b973d878907757593ff6c
Simon van der Veldt <simon.vanderveldt@gmail.com>
2022-07-11 21:21:13 +0200

    portage: Switch to 17.1 profile
    As preparation for switching to Gentoo's GNOME, 17.0 isn't supported anymore in upstream Gentoo so have to switch to 17.1

--- portage/init.sh
+++ portage/init.sh
@@ -4,4 +4,4 @@ set -x
 # Sync first to ensure profiles from (new) overlays are present
 emerge --sync
 
-eselect profile set dantrell-gnome-3-38:default/linux/amd64/17.0/desktop/gnome/3.38
+eselect profile set dantrell-gnome-3-38:default/linux/amd64/17.1/desktop/gnome/3.38
