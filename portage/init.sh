aaef1f0d7e0fc38411808258d9fec562aa7633e1
Simon van der Veldt <simon.vanderveldt@gmail.com>
2024-12-04 21:58:42 +0100

    portage: Upgrade to profile 23.0

--- portage/init.sh
+++ portage/init.sh
@@ -4,4 +4,4 @@ set -x
 # Sync first to ensure profiles from (new) overlays are present
 emerge --sync
 
-eselect profile set default/linux/amd64/17.1/desktop/gnome
+eselect profile set default/linux/amd64/23.0/split-usr/desktop/gnome
